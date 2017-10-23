d34dd657d90b30fc0e36b7628436680412b5bdbe
Chomyczek <kaszubowski.adam@gmail.com>
2017-10-23 23:38:31 +0200

    add app.cs

---
+++ XamarinExchangerCalc/ExchangerCalc.Core/App.cs
@@ -0,0 +1,32 @@
+﻿// XamarinExchangerCalc
+// Copyright(C) 2017
+// Author Adam Kaszubowski
+
+#region Usings
+
+using ExchangerCalc.Core.Services;
+
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+
+#endregion
+
+namespace ExchangerCalc.Core
+{
+	public class App : MvxApplication
+	{
+		#region Constructors and Destructors
+
+		public App()
+		{
+			Mvx.RegisterType<ICalculator, Calculator>();
+			var calculator = Mvx.Resolve<ICalculator>();
+
+			//var appStart = new MvxAppStart<CalculatorViewModel>();
+			//var appStart = new MvxNavigationService(CalculatorViewModel);
+			//Mvx.RegisterSingleton<();
+		}
+
+		#endregion
+	}
+}
