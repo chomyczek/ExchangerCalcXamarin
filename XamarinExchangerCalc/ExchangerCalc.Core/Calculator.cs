5f6698768836d67e3df90c5a04226b00e4deb70e
Chomyczek <kaszubowski.adam@gmail.com>
2017-10-23 22:55:24 +0200

    add base calculator

---
+++ XamarinExchangerCalc/ExchangerCalc.Core/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangerCalc.Core
+{
+	public class Calculator: ICalculator
+	{
+		public double Calculate(double carbohydrate, double weight, double insulin)
+		{
+			return (carbohydrate / 10) * (weight / 100) * insulin;
+		}
+	}
+}
---
+++ XamarinExchangerCalc/ExchangerCalc.Core/ICalculator.cs
@@ -0,0 +1,15 @@
+﻿// XamarinExchangerCalc
+// Copyright(C) 2017
+// Author Adam Kaszubowski
+
+namespace ExchangerCalc.Core
+{
+	public interface ICalculator
+	{
+		#region Public Methods and Operators
+
+		double Calculate(double carbohydrate,double weight, double insulin);
+
+		#endregion
+	}
+}
