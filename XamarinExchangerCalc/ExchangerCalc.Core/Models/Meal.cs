ae957518f04b1e30e4b726b9a1b1e760e2c33a77
Chomyczek <kaszubowski.adam@gmail.com>
2018-01-30 20:00:17 +0100

    add repository pattern and update meal model

--- XamarinExchangerCalc/ExchangerCalc.Core/Models/Meal.cs
+++ XamarinExchangerCalc/ExchangerCalc.Core/Models/Meal.cs
@@ -1,30 +1,54 @@
 ﻿// XamarinExchangerCalc
-// Copyright(C) 2017
+// Copyright(C) 2018
 // Author Adam Kaszubowski
 
 #region Usings
 
 using ExchangerCalc.Core.Enums;
 
+using SQLite;
+
 #endregion
 
 namespace ExchangerCalc.Core.Models
 {
+	[Table(nameof(Meal))]
 	public class Meal
 	{
+		#region Constructors and Destructors
+
+		public Meal()
+		{
+			this.Carbohydrate = 0;
+			this.Weight = 100;
+			this.Fat = 0;
+			this.Name = string.Empty;
+			this.Protein = 0;
+			this.UnitMeasure = Unit.Grams;
+			this.IsPreDefined = false;
+		}
+
+		#endregion
+
 		#region Public Properties
 
 		public double Carbohydrate { get; set; }
 
-		public double Weight { get; set; }
-
 		public double Fat { get; set; }
 
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+
+		public bool IsPreDefined { get; set; }
+
+		[NotNull]
 		public string Name { get; set; }
 
 		public double Protein { get; set; }
 
 		public Unit UnitMeasure { get; set; }
+
+		public double Weight { get; set; }
 
 		#endregion
 	}
---
+++ XamarinExchangerCalc/ExchangerCalc.Core/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ExchangerCalc.Core.Models;
+
+using SQLite;
+
+namespace ExchangerCalc.Core
+{
+	public class Repository
+	{
+		private readonly SQLiteAsyncConnection conn;
+
+		public Repository(string dbPath)
+		{
+			this.conn = new SQLiteAsyncConnection(dbPath);
+			this.PrepareDb();
+		}
+
+		private void PrepareDb()
+		{
+			this.conn.CreateTableAsync<Meal>().Wait();
+		}
+
+		public async void CreateMeal(Meal meal)
+		{
+			await this.conn.InsertAsync(meal).ConfigureAwait(false);
+		}
+
+		public Task<List<Meal>> GetAllMeals()
+		{
+			return this.conn.Table<Meal>().ToListAsync();
+		}
+
+	}
+}
